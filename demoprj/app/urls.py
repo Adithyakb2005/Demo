a56173408fb1058b19098036e656104e9187be1d
Adithyakb2005 <adithyakb1001@gmail.com>
2024-11-21 11:32:42 +0530

    new cart add

--- demoprj/app/urls.py
+++ demoprj/app/urls.py
@@ -5,9 +5,13 @@ urlpatterns=[
     path('',views.e_shop_login),
     path('home',views.shop_home),
     path('logout',views.e_shop_logout),
-    path('addproducts',views.addproducts),
+    path('add',views.addproduct),
     path('editproduct/<pid>',views.editproduct),
     path('deleteproduct/<pid>',views.deleteproduct),
-    # path('register',views.register),
+    path('register',views.register),
+    path('user_home',views.user_home),
+    path('product_view/<pid>',views.product_view),
+    path('add_to_cart/<pid>',views.add_to_cart),
+    path('view_cart',views.view_cart)
 
 ]
--- demoprj/app/views.py
+++ demoprj/app/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .models import Product
+from .models import *
 import os
+from django.contrib.auth.models import User
 # Create your views here.
 def e_shop_login(req):
     if 'shop' in req.session:
         return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
     if req.method=='POST':
         username=req.POST['username']
         password=req.POST['password']
         data=authenticate(username=username,password=password)
         if data:
             login(req,data)
-            return redirect(shop_home)
+            if data.is_superuser:
+                req.session['shop']=username #create
+                return redirect(shop_home)
+            else:
+                req.session['user']=username 
+                return redirect(user_home)
         else:
             messages.warning(req, "Invalid username or password.")
             return redirect(e_shop_login)
@@ -24,48 +32,47 @@ def e_shop_logout(req):
     logout(req)
     req.session.flush()
     return redirect(e_shop_login)
+#---------------------admin------------------------
 def shop_home(req):
     return render(req,'shop/home.html')
 
-def addproducts(req) :
+def addproduct(req) :
     if 'shop' in req.session:
         if req.method=='POST':
             pid=req.POST['pid']
-            pname=req.POST['name']
-            des=req.POST['descrip']
-            pprice=req.POST['price']
-            oprice=req.POST['off_price']
-            pstock=req.POST['stock']
+            name=req.POST['name']
+            dis=req.POST['descrip']
+            price=req.POST['price']
+            stock=req.POST['stock']
             file=req.FILES['img'] #to get the img from html page we  are using file method instead of post and adding enctype in html page
 
-            data=Product.objects.create(pid=pid,name=pname,des=des,price=pprice,offer_price=oprice,stock=pstock,img=file)
+            data=Product.objects.create(pid=pid,name=name,dis=dis,price=price,stock=stock,img=file)
             data.save()
             return redirect(shop_home)
         else:
-            return render(req,'shop/add_products.html')
+            return render(req,'shop/addproduct.html')
     else:
         return redirect(e_shop_login)    
     
 def editproduct(req,pid) :
         if req.method=='POST':
             proid=req.POST['proid']
-            pname=req.POST['name']
-            des=req.POST['descrip']
-            pprice=req.POST['price']
-            oprice=req.POST['off_price']
-            pstock=req.POST['stock']
+            name=req.POST['name']
+            dis=req.POST['descrip']
+            price=req.POST['price']
+            stock=req.POST['stock']
             file=req.FILES.get('img')
             if file:
-                Product.objects.filter(pk=pid).update(pid=proid,name=pname,des=des,price=pprice,offer_price=oprice,stock=pstock,img=file)
+                Product.objects.filter(pk=pid).update(pid=proid,name=name,dis=dis,price=price,stock=stock,img=file)
                 data=Product.objects.get(pk=pid)
                 data.img=file
                 data.save()
             else:  
-                Product.objects.filter(pk=pid).update(pid=pid,name=pname,des=des,price=pprice,offer_price=oprice,stock=pstock,img=file)
+                Product.objects.filter(pk=pid).update(pid=pid,name=name,dis=dis,price=price,stock=stock,img=file)
             return redirect(shop_home)
         else:
             data=Product.objects.get(pk=pid)
-            return render(req,'shop/edit_product.html',{'data':data})
+            return render(req,'shop/edit.html',{'data':data})
 
 def deleteproduct(req,pid):
     data=Product.objects.get(pk=pid)
@@ -74,5 +81,39 @@ def deleteproduct(req,pid):
     os.remove('media/'+file)
     data.delete()
     return redirect(shop_home)
-# def register(req):
-#     return render(req,'register.html')
+#--------------------------------user--------------------
+def register(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        email=req.POST['email']
+        pswd=req.POST['pswd']
+        try:
+            data=User.objects.create_user(first_name=uname,email=email,username=email,password=pswd)
+            data.save()
+        except:
+            messages.warning(req,"email already in use")
+            return redirect(register)
+
+        return redirect(e_shop_login)
+    else:
+        return render(req,'user/register.html')
+def user_home(req):
+    if 'user' in req.session:
+        data=Product.objects.all()
+        return render(req,'user/userhome.html',{'product':data})
+    else:
+        return redirect(e_shop_login)
+
+def product_view(req,pid):
+       data=Product.objects.get(pk=pid)
+       return render(req,'user/product_view.html',{'product':data})
+    
+
+def add_to_cart(req,pid):
+    product=Product.objects.get(pk=pid)
+    user=User.objects.get(username=req.session['user'])
+    data=Cart.objects.create(product=product,user=user,qty=1)
+    data.save()
+    return  redirect(view_cart)
+def view_cart(req):
+    return render(req,'user/cart.html')
