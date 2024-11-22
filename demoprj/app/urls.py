fc5920eb862fcf4259e6ad8314241a7376f7f23e
Adithyakb2005 <adithyakb1001@gmail.com>
2024-11-22 11:28:59 +0530

    addto cart add

--- demoprj/app/urls.py
+++ demoprj/app/urls.py
@@ -12,6 +12,8 @@ urlpatterns=[
     path('user_home',views.user_home),
     path('product_view/<pid>',views.product_view),
     path('add_to_cart/<pid>',views.add_to_cart),
-    path('view_cart',views.view_cart)
+    path('view_cart',views.view_cart),
+    path('qty_in/<cid>',views.qty_in),
+    path('qty_dec/<cid>',views.qty_dec)
 
 ]
